1520da1210b4e3bef053f24fb7b61be4f61be964
Alematri <alessandromatricardi@gmail.com>
2023-10-31 10:28:25 +0100

    [Bonus 1,2 e 3] Done!

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 const { createApp } = Vue;
 
 createApp({
-
-  data(){
-    return{
-    apiUrl: 'server.php',
-    title: 'ToDoList',
-    list: [],
-    newTodo: ''
-    }
+  data() {
+    return {
+      apiUrl: 'server.php',
+      title: 'ToDoList',
+      list: [],
+      newTodo: '',
+    };
   },
-
-  methods:{
-    getList(){
-      axios.get(this.apiUrl)
-        .then(result =>{
-          this.list = result.data;
-        })
+  methods: {
+    getList() {
+      axios.get(this.apiUrl).then((result) => {
+        this.list = result.data;
+      });
     },
-    addTask(){
-      // il dato che invio al server, per essere accettato da PHP come se provenisse da un form quindi devo crearlo dentro un FormData
+    addTask() {
+      // Il dato che invio al server, per essere accettato da PHP come se provenisse da un form, quindi devo crearlo dentro un FormData
 
       const data = new FormData();
       data.append('todoItem', this.newTodo);
 
-      axios.post(this.apiUrl, data)
-        .then(result =>{
-          this.list = result.data;
-          this.newTodo = '';
-        })
+      axios.post(this.apiUrl, data).then((result) => {
+        this.list = result.data;
+        this.newTodo = '';
+      });
     },
-    removeTask(index){
+    removeTask(index) {
       const data = new FormData();
       data.append('indexToDelete', index);
 
-      axios.post(this.apiUrl, data)
-        .then(result =>{
-          this.list = result.data;
-        })
-    }
+      axios.post(this.apiUrl, data).then((result) => {
+        this.list = result.data;
+      });
+    },
+    toggleTask(index) {
+      this.list[index].completed = !this.list[index].completed;
+
+      const data = new FormData();
+      data.append('indexToToggle', index);
+
+      axios.post(this.apiUrl, data).then((result) => {
+        this.list = result.data;
+      });
+    },
   },
-
-  mounted(){
+  mounted() {
     this.getList();
-  }
-
+  },
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,39 +1,43 @@
 <?php
 
-//1 leggo il JSON e lo salvo in una variabile sotto forma di stringa
+// 1. Leggo il JSON e lo salvo in una variabile sotto forma di stringa
 $json_string = file_get_contents('todo-list.json');
-// il metodo file_get_contents prende un file esterno e lo restituisce sottoforma di stringa
 
-//2 trasformo la stringa in un array PHP
+// 2. Trasformo la stringa in un array PHP
 $list = json_decode($json_string, true);
-// il metodono json_decode trasforma la stringa in un array PHP appunto
 
-// verifico se mi arriva in post la variabile del nuovo item
-if(isset($_POST['todoItem'])){
-  //aggiungo l'elemento alla lista
+// Verifico se mi arriva in post la variabile del nuovo item
+if (isset($_POST['todoItem'])) {
+  // Aggiungo l'elemento alla lista
   $newItem = $_POST['todoItem'];
+  $list[] = array('text' => $newItem, 'completed' => false);
 
-  $list[] = $newItem;
-  // salvo il dato nel file JSON esterno insieme a tutta la lista aggiornata
+  // Salvo il dato nel file JSON esterno insieme a tutta la lista aggiornata
   file_put_contents('todo-list.json', json_encode($list));
-  // file_put_contents è un metodo nativo di PHP che va a scrivere del testo all'interno di una stringa. In questo caso dentro todo-list ci vado a scrivere $list trasformata in stringa da json_encode
 }
 
-// verifico se c'è un indice di un item da eliminare
-if(isset($_POST['indexToDelete'])){
-
+// Verifico se c'è un indice di un item da eliminare
+if (isset($_POST['indexToDelete'])) {
   $indexToDelete = $_POST['indexToDelete'];
-  //elimino l'elemento dall'array in base all'indicer passato
+
+  // Elimino l'elemento dall'array in base all'indice passato
   array_splice($list, $indexToDelete, 1);
-  //array_splice è un metodo che toglie dall'array che gli passo (list), partendo da indice che gli passo (indextodelete), un certo quantitativo di elementi (1)
-  // salvo la lista aggiornata nel JSON
+
+  // Salvo la lista aggiornata nel JSON
   file_put_contents('todo-list.json', json_encode($list));
 }
 
-//3 "trasformo" il file PHP in file JSON
-header('Content-Type: application/json');
-// Content-type modifica l'header del file trasformandolo in json
+// Verifico se c'è un indice di un item da togglare
+if (isset($_POST['indexToToggle'])) {
+  $indexToToggle = $_POST['indexToToggle'];
+  $list[$indexToToggle]['completed'] = !$list[$indexToToggle]['completed'];
 
-//4 stampo la lista ricodificata, la lista PHP torna ad essere una stringa e viene stampata
+  // Salvo la lista aggiornata nel JSON
+  file_put_contents('todo-list.json', json_encode($list));
+}
+
+// 3. "Trasformo" il file PHP in file JSON
+header('Content-Type: application/json');
+
+// 4. Stampo la lista ricodificata, la lista PHP torna ad essere una stringa e viene stampata
 echo json_encode($list);
-// il metodo json_econde fa il processo inverso di json_decode
